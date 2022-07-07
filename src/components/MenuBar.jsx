a621cd46f4f18dffdbe5fad53e3ff2644c64221e
Daniel Lewis <dlew@macbook-air.myfiosgateway.com>
2022-07-07 10:16:18 -0400

    add socials to header

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import MenuBarItem from "./MenuBarItem";
 import DropdownMenu from "./DropdownMenu";
 import { FiChevronDown } from "react-icons/fi";
+import { BsMedium, BsTwitter, BsStackOverflow } from "react-icons/bs";
+
 
 function MenuBar(props) {
   return (
@@ -10,7 +12,20 @@ function MenuBar(props) {
         <img className="menu-bar__logo" src="./img/dndl-logo.png" alt="logo" />
       </a>
 
-      <span className="menu-bar__header">Daniel (not Day) Lewis</span>
+      <div className="menu-bar__header">Daniel (not Day) Lewis
+      
+        <div className="menu-bar__socials">
+          
+          <a href="https://medium.com/@daniel_notday_lewis" target="_blank"><BsMedium /></a>
+          <a href="https://twitter.com/daniel_nd_lewis" target="_blank"><BsTwitter /></a>
+          <a href="https://stackoverflow.com/users/17514943/daniel" target="_blank"><BsStackOverflow /></a>
+
+
+        </div>
+      
+      </div>
+
+
 
       <ul className="menu-bar__links">
         <MenuBarItem icon={<FiChevronDown />}>
