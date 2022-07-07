6300a6a6472612a87695785aea78f4dcafb3b4a4
Daniel Lewis <dlew@macbook-air.myfiosgateway.com>
2022-07-06 23:26:30 -0400

    fuck my pussy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { BsStackOverflow, BsTwitter, BsMedium } from "react-icons/bs";
 
 const date = new Date();
 const year = date.getFullYear();
@@ -12,38 +12,51 @@ function Footer() {
     
         <li className="footer__nav__item">
           <a
-            href="https://github.com/kondaguey"
+            href="https://stackoverflow.com/users/17514943/daniel"
             className="footer__nav__item-link"
             target="_blank"
             rel="noreferrer"
           >
             <span className="footer__nav__item-logo">
-              <AiFillGithub />
+              <BsStackOverflow />
             </span>
-            Github
+            Stack Overflow
           </a>
         </li>
         <li className="footer__nav__item">
           <a
-            href="https://www.linkedin.com/in/daniel-lewis-55a903195/"
+            href="https://twitter.com/daniel_nd_lewis"
             target="_blank"
             className="footer__nav__item-link"
             rel="noreferrer"
           >
             <span className="footer__nav__item-logo">
-              <AiFillLinkedin />
+              <BsTwitter />
             </span>
-            LinkedIn
+            Twitter
           </a>
         </li>
         <li className="footer__nav__item">
-          <a href="/privacypolicy" className="footer__nav__item-link">
-            Disclaimers
+          <a
+            href="https://medium.com/@daniel_notday_lewis"
+            target="_blank"
+            className="footer__nav__item-link"
+            rel="noreferrer"
+          >
+            <span className="footer__nav__item-logo">
+              <BsMedium />
+            </span>
+            Medium
           </a>
         </li>
         <li className="footer__nav__item">
           <a href="/collab" className="footer__nav__item-link">
             Collab
+          </a>
+        </li>
+        <li className="footer__nav__item">
+          <a href="/privacypolicy" className="footer__nav__item-link">
+            Disclaimers
           </a>
         </li>
       </ul>
