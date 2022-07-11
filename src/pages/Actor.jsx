433d430aad8d4e6d6dd00d4f10eb033f6f1cc36a
Daniel Lewis <dlew@macbook-air.myfiosgateway.com>
2022-07-11 12:24:37 -0400

    add findway

--- src/pages/Actor.jsx
+++ src/pages/Actor.jsx
@@ -3,18 +3,36 @@ import React from "react";
 function Actor() {
   return (
     <div className="actor">
-
-
-
       <div className="actor__header">
-        <div className="actor__header-title"><h1>/actor</h1></div>
-        <span><a className="actor__header-resume" href="./media/ActorResume.pdf" target="_blank"><h2>resume</h2></a></span>
+        <div className="actor__header-title">
+          <h1>/actor</h1>
+        </div>
+        <span>
+          <a
+            className="actor__header-resume"
+            href="./media/ActorResume.pdf"
+            target="_blank"
+          >
+            <h2>resume</h2>
+          </a>
+        </span>
       </div>
 
       <p className="actor__paragraph">
-        I am still accepting some professional voiceover work. Otherwise this dude is on indefinite hiatus as I work on other career aspirations (i.e. development and design). <br /><br />But do check out my resume above if you're curious. {" "}
+        I am still accepting some{" "}
+        <a href="#voiceactor">professional voiceover work</a>. Otherwise this
+        dude is on indefinite hiatus as I work on other career aspirations (i.e.
+        development and design). <br />
+        <br />
+        But do check out my resume above if you're curious.{" "}
       </p>
-    
+
+      <div className="actor__voiceactor" id="voiceactor">
+        <h1>/voiceactor</h1>
+        <a href="https://my.findawayvoices.com/narrator/dndl?reviewer=7641" target="_blank">
+          <img src="img/findaway.svg" alt="findaway voices profile" />
+        </a>
+      </div>
     </div>
   );
 }
